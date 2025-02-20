6310cb338d4ca77b66f341dc51e253a7722ee5c9
Alexander Weinrauch <alexander.weinrauch@amd.com>
2025-02-20 12:24:33 +0000

    Cleanup

--- third_party/amd/lib/TritonAMDGPUToLLVM/TargetInfo.h
+++ third_party/amd/lib/TritonAMDGPUToLLVM/TargetInfo.h
@@ -75,6 +75,8 @@ public:
   void storeOpAnnotation(triton::gpu::LocalStoreOp op, size_t localStoreOpCount,
                          Type type) const override;
 
+  SmallVector<unsigned> supportedLoadWidthsToLds() const;
+
 private:
   void printfImpl(Value formatStrStart, int formatStrByteCount, ValueRange args,
                   RewriterBase &rewriter, bool useStdErr) const;
