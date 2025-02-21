a2505ebd81225fb629c24f9067fc825385936500
Alexander Weinrauch <alexander.weinrauch@amd.com>
2025-02-21 10:56:25 +0000

    Add swizzle support for buffer loads to lds

--- third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
@@ -454,51 +454,179 @@ struct BufferLoadToLocalOpConversion
     // buffer_load into LDS does not support per lane offsets.
     // We need to ensure that we write coalesced into shared memory.
     auto dstTy = op.getResult().getType();
+    bool isSharedSwizzled = false;
     if (!LLVM::AMD::writesCoalscedIntoLocalMemory(rewriter, ptrType, dstTy,
                                                   vec)) {
-      return rewriter.notifyMatchFailure(op,
-                                         "does not write coalesced into LDS");
+      isSharedSwizzled = true;
     }
+    auto shape = dstTy.getShape();
+    LinearLayout srcLayout =
+        triton::gpu::toLinearLayout(shape, ptrType.getEncoding());
+    LinearLayout sharedLayout =
+        triton::gpu::toLinearLayout(shape, dstTy.getEncoding());
+    LinearLayout srcToSharedLayout = srcLayout.invertAndCompose(sharedLayout);
 
-    // Create the resource descriptor and then emit the buffer_loads to lds
-    Value rsrcDesc = bufferEmitter.createResourceDescriptor(llPtr, llStride);
+    if (!isSharedSwizzled) {
+      // Create the resource descriptor and then emit the buffer_loads to lds
+      Value rsrcDesc = bufferEmitter.createResourceDescriptor(llPtr, llStride);
 
-    auto resElemTy = getTypeConverter()->convertType(dstTy.getElementType());
-    auto smemObj = mlir::LLVM::getSharedMemoryObjectFromStruct(
-        loc, llDst, resElemTy, rewriter);
+      auto resElemTy = getTypeConverter()->convertType(dstTy.getElementType());
+      auto smemObj = mlir::LLVM::getSharedMemoryObjectFromStruct(
+          loc, llDst, resElemTy, rewriter);
 
-    VectorType vecTy;
-    SmallVector<Value> shmemAddrs;
-    bool ok = emitTransferBetweenRegistersAndShared(
-        ptrType, dstTy, resElemTy, {}, smemObj, loc, rewriter, targetInfo,
-        [&](VectorType vecTy_, Value shmemAddr) {
-          vecTy = vecTy_;
-          shmemAddrs.push_back(shmemAddr);
-        });
-    assert(ok);
+      VectorType vecTy;
+      SmallVector<Value> shmemAddrs;
+      bool ok = emitTransferBetweenRegistersAndShared(
+          ptrType, dstTy, resElemTy, {}, smemObj, loc, rewriter, targetInfo,
+          [&](VectorType vecTy_, Value shmemAddr) {
+            vecTy = vecTy_;
+            shmemAddrs.push_back(shmemAddr);
+          });
+      assert(ok);
 
-    int vecBits = vecTy.getNumElements() * vecTy.getElementTypeBitWidth();
-    if (!llvm::is_contained(targetInfo.supportedLoadWidthsToLds(), vecBits)) {
-      return rewriter.notifyMatchFailure(
-          op, "Async copy does not support the required load vectorization");
-    }
+      int vecBits = vecTy.getNumElements() * vecTy.getElementTypeBitWidth();
+      if (!llvm::is_contained(targetInfo.supportedLoadWidthsToLds(), vecBits)) {
+        return rewriter.notifyMatchFailure(
+            op, "Async copy does not support the required load vectorization");
+      }
 
-    int vecBytes = vecBits / 8;
-    assert(llvm::isPowerOf2_32(vecBytes));
-    Value vecBytesVal = b.i32_val(vecBytes);
+      int vecBytes = vecBits / 8;
+      assert(llvm::isPowerOf2_32(vecBytes));
+      Value vecBytesVal = b.i32_val(vecBytes);
 
-    for (int i = 0; i < shmemAddrs.size(); i++) {
-      auto srcIdx = i * vec;
-      auto offsetIn = offsetElems[srcIdx];
+      for (int i = 0; i < shmemAddrs.size(); i++) {
+        auto srcIdx = i * vec;
+        auto offsetIn = offsetElems[srcIdx];
 
-      Value pred = mask ? maskElems[srcIdx] : b.int_val(1, 1);
-      bufferEmitter.emitLoadToLds(vecTy, vecBytesVal, rsrcDesc, offsetIn,
-                                  shmemAddrs[i], pred, op.getCache());
-      if (!otherElems.empty()) {
-        Value storeVal = packElementRangeIntoVector(
-            rewriter, this->getTypeConverter(), loc, vecTy, otherElems, srcIdx);
-        llStore(rewriter, loc, shmemAddrs[i], storeVal,
-                b.icmp_ne(maskElems[srcIdx], b.true_val()), op.getCache());
+        Value pred = mask ? maskElems[srcIdx] : b.int_val(1, 1);
+        bufferEmitter.emitLoadToLds(vecTy, vecBytesVal, rsrcDesc, offsetIn,
+                                    shmemAddrs[i], pred, op.getCache());
+        if (!otherElems.empty()) {
+          Value storeVal =
+              packElementRangeIntoVector(rewriter, this->getTypeConverter(),
+                                         loc, vecTy, otherElems, srcIdx);
+          llStore(rewriter, loc, shmemAddrs[i], storeVal,
+                  b.icmp_ne(maskElems[srcIdx], b.true_val()), op.getCache());
+        }
+      }
+    } else {
+      // Create the resource descriptor and then emit the buffer_loads to lds
+      Value rsrcDesc = bufferEmitter.createResourceDescriptor(llPtr, llStride);
+
+      auto resElemTy = getTypeConverter()->convertType(dstTy.getElementType());
+      auto smemObj = mlir::LLVM::getSharedMemoryObjectFromStruct(
+          loc, llDst, resElemTy, rewriter);
+
+      VectorType vecTy;
+      SmallVector<Value> shmemAddrs;
+      bool ok = emitTransferBetweenRegistersAndShared(
+          ptrType, dstTy, resElemTy, {}, smemObj, loc, rewriter, targetInfo,
+          [&](VectorType vecTy_, Value shmemAddr) {
+            vecTy = vecTy_;
+            shmemAddrs.push_back(shmemAddr);
+          });
+      assert(ok);
+
+      llvm::outs() << "Num shmemAddrs: " << shmemAddrs.size() << "\n";
+      llvm::outs() << "VecTy: ";
+      vecTy.print(llvm::outs());
+      llvm::outs() << "\n";
+
+      // We can only apply the trick if we do not swizzle across warp boundaries
+      // This means the lane dimensions has to be smaller than 2^lane_dim - 1
+      StringAttr kLane = rewriter.getStringAttr("lane");
+      for (int inLane : llvm::seq(srcToSharedLayout.getInDimSizeLog2(kLane))) {
+        auto basis = srcToSharedLayout.getBasis(kLane, inLane)[0];
+        unsigned upperLimit = vec * ((1 << (inLane + 1)) - 1);
+        llvm::outs() << "Upper limit: " << inLane << "= " << upperLimit
+                     << " with basis= " << basis << "\n";
+        if (basis > upperLimit) {
+          LDBG("detected swizzling with crosses warp boundaries in async "
+               "copy "
+               "for lane "
+               << 1 + inLane << "; given " << basis
+               << " but should be smaller or equal to " << upperLimit);
+          assert("Swizzling across lane boundaries" && false);
+        }
+      }
+
+      int vecBits = vecTy.getNumElements() * vecTy.getElementTypeBitWidth();
+      if (!llvm::is_contained(targetInfo.supportedLoadWidthsToLds(), vecBits)) {
+        return rewriter.notifyMatchFailure(
+            op, "Async copy does not support the required load vectorization");
+      }
+
+      int vecBytes = vecBits / 8;
+      assert(llvm::isPowerOf2_32(vecBytes));
+      Value vecBytesVal = b.i32_val(vecBytes);
+
+      Value cacheModifiers =
+          b.i32_val(mlir::LLVM::AMD::getCtrlBitsForCacheModifierOnTarget(
+              op.getCache(), /*isLoad=*/true, targetInfo));
+
+      // Value other = op.getOther();
+      // SmallVector<Value> otherElems;
+      // if (other) {
+      //   otherElems = unpackLLElements(loc, adaptor.getOther(), rewriter);
+      //   assert(srcElems.size() == otherElems.size());
+      // }
+
+      // The idea is to modify the dst layout to a colaseced one and instead
+      // move the swizzling to the src layout
+      auto sharedEncoding =
+          dyn_cast<SwizzledSharedEncodingAttr>(dstTy.getEncoding());
+      auto flattenDst = SwizzledSharedEncodingAttr::get(
+          op->getContext(), sharedEncoding.getVec(), 1, 1,
+          sharedEncoding.getOrder(), sharedEncoding.getCTALayout());
+      LinearLayout flattenDstLayout =
+          triton::gpu::toLinearLayout(shape, dstTy.getEncoding());
+      LinearLayout srcToFlattenLayout =
+          srcLayout.invertAndCompose(sharedLayout);
+
+      VectorType flattenVecTy;
+      SmallVector<Value> flattenShmemAddrs;
+      auto flattenTy = triton::gpu::MemDescType::get(
+          dstTy.getShape(), dstTy.getElementType(), flattenDst,
+          dstTy.getMemorySpace());
+      llvm::outs() << "Flatten ty: ";
+      flattenTy.dump();
+      llvm::outs() << "\n";
+      ok = emitTransferBetweenRegistersAndShared(
+          ptrType, flattenTy, resElemTy, {}, smemObj, loc, rewriter, targetInfo,
+          [&](VectorType vecTy_, Value shmemAddr) {
+            flattenVecTy = vecTy_;
+            flattenShmemAddrs.push_back(shmemAddr);
+          });
+      assert(ok);
+
+      assert(flattenShmemAddrs.size() == shmemAddrs.size());
+      assert(flattenVecTy == vecTy);
+
+      for (int i = 0; i < shmemAddrs.size(); i++) {
+        auto srcIdx = i * vec;
+        auto offsetIn = offsetElems[srcIdx];
+
+        // We need to modify srcPtr by the difference between the dstLayout
+        // and the flatteneddstLayout, we can take a shortcut by using the
+        // shmemAddresses
+        auto src1Ptr = b.ptrtoint(i32_ty, shmemAddrs[i]);
+        auto src2Ptr = b.ptrtoint(i32_ty, flattenShmemAddrs[i]);
+        int elementBytes = vecBytes / vecTy.getNumElements();
+        Value diff = b.sdiv(b.sub(src1Ptr, src2Ptr), b.i32_val(elementBytes));
+        auto newOffset = b.add(offsetIn, diff);
+        // auto newSrcPtr =
+        //     b.gep(srcPtr.getType(), dstTy.getElementType(), srcPtr, diff);
+
+        Value pred = mask ? maskElems[srcIdx] : b.int_val(1, 1);
+        bufferEmitter.emitLoadToLds(vecTy, vecBytesVal, rsrcDesc, newOffset,
+                                    flattenShmemAddrs[i], pred, op.getCache());
+        if (!otherElems.empty()) {
+          Value storeVal =
+              packElementRangeIntoVector(rewriter, this->getTypeConverter(),
+                                         loc, vecTy, otherElems, srcIdx);
+          llStore(rewriter, loc, flattenShmemAddrs[i], storeVal,
+                  b.icmp_ne(maskElems[srcIdx], b.true_val()), op.getCache());
+        }
       }
     }
 
