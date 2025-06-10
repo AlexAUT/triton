027b949f9148262857c5e9b544f13007e886d243
Alexander Weinrauch <alexander.weinrauch@amd.com>
2025-06-10 09:45:00 +0000

    Adjust bench_mlp to reproduce issue

--- third_party/proton/proton/specs.py
+++ third_party/proton/proton/specs.py
@@ -13,6 +13,7 @@ flops_by_device = {
     "HIP": {
         "gfx90a": lambda width, **kwargs: 383e12 / (width / 8),
         "gfx942": lambda width, **kwargs: 2614.9e12 / (width / 8),
+        "gfx950": lambda width, **kwargs: 2614.9e12 / (width / 8),
     },
 }
 
