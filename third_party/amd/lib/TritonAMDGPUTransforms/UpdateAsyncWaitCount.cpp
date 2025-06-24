041e7a6a9f14131fd32195507724e5abb81b395f
Alexander Weinrauch <alexander.weinrauch@amd.com>
2025-06-24 16:58:17 +0000

    Fixed order in LL calculation

--- third_party/amd/lib/TritonAMDGPUTransforms/UpdateAsyncWaitCount.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/UpdateAsyncWaitCount.cpp
@@ -34,7 +34,7 @@ namespace {
 int getNumberOfLoadInstructions(RankedTensorType srcTy,
                                 ttg::MemDescType dstTy) {
   LinearLayout srcToSharedLayout =
-      triton::gpu::getRegToSharedLayout(srcTy, dstTy);
+      triton::gpu::getRegToSharedLayoutForPadding(srcTy, dstTy);
 
   // On GFX9 we cannot split direct to lds loads into multiple ones because we
   // need coalesced writes. So we can divide the number of registers by the
